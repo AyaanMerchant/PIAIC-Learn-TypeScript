b4ad99c767969c56fd98dacdd81a2c0786077b4a
Ayaan Merchant <krmkae786@gmail.com>
2024-05-26 18:13:35 +0500

    ATM Machine CLI project Completed

---
+++ Quarter-01/Projects/ATM-Machine/index.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let balance = 5000;
+let myPin = 1234;
+
+console.log("\n\tWelcome to 'CodeWithAyaan' - ATM Machine\n");
+
+let pinAnswer = await inquirer.prompt([
+  {
+    name: "pin",
+    type: "number",
+    message: "\nEnter Your Pin Code:",
+  },
+]);
+
+if (pinAnswer.pin === myPin) {
+  console.log("\nPin is Correct, Login Successfully!");
+  console.log(`Your Current Balance is ${balance}`);
+
+  let operationAnswer = await inquirer.prompt([
+    {
+      name: "operation",
+      type: "list",
+      message: "\nSelect the Operation",
+      choices: ["Withdraw Amount", "Check Balance"],
+    },
+  ]);
+
+  if (operationAnswer.operation === "Withdraw Amount") {
+    let amountAnswer = await inquirer.prompt([
+      {
+        name: "amount",
+        type: "number",
+        message: "Enter Amount to Withdraw:",
+      },
+    ]);
+    if (amountAnswer.amount > balance) {
+      console.log("\nInsufficient Balance");
+    } else {
+      balance -= amountAnswer.amount;
+      console.log(`\n${amountAnswer.amount} WithDraw Sucessfully!`);
+      console.log(`Your Remainning Balance is ${balance}`);
+      console.log("Thank You For Transaction");
+    }
+  } else if (operationAnswer.operation === "Check Balance") {
+    console.log(`\nYour Acount Balance is ${balance}`);
+  }
+} else {
+  console.log("\nYou Have Enter Wrong Pin! Try Again");
+}
