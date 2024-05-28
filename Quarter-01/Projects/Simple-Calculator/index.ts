e95b1ddcf8d7a1656be3d683d8f697a6d9e14625
Ayaan Merchant <krmkae786@gmail.com>
2024-05-28 15:18:38 +0500

    Use Chalk Libary to add colors in the code

--- Quarter-01/Projects/Simple-Calculator/index.ts
+++ Quarter-01/Projects/Simple-Calculator/index.ts
@@ -1,28 +1,32 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(`\t\t<<<<======================================================>>>>`)
+console.log(chalk.cyan.bold(`\t\t\tWelcome To CodeWithAyaan - Simple Calculator`))
+console.log(`\t\t<<<<======================================================>>>>\n`)
 
 const answer = await inquirer.prompt([
-  { message: "Enter First Number", type: "number", name: "fistNumber" },
-  { message: "Enter Second Number", type: "number", name: "secondNumber" },
+  { message: chalk.yellow.bold("Enter First Number:"), type: "number", name: "fistNumber" },
+  { message: chalk.yellow.bold("Enter Second Number:"), type: "number", name: "secondNumber" },
   {
-    message: "Select Operator to Perform Action",
+    message: chalk.green("Select Operator to Perform Action: "),
     type: "list",
     name: "Operator",
     choices: ["Addition", "Subtraction", "Multiplication", "Division"],
   },
 ]);
 
-console.log(answer);
+console.log(answer); 
 //conditional Statement
 if (answer.Operator === "Addition") {
-  console.log(answer.fistNumber + answer.secondNumber);
+  console.log(`\nAnswer: ${chalk.magenta.bold(answer.fistNumber)} + ${chalk.magenta.bold(answer.secondNumber)} =` , chalk.green.bold(answer.fistNumber + answer.secondNumber));
 } else if (answer.Operator === "Subtraction") {
-  console.log(answer.fistNumber - answer.secondNumber);
+  console.log(`\nAnswer: ${chalk.magenta.bold(answer.fistNumber)} - ${chalk.magenta.bold(answer.secondNumber)} =` , chalk.green.bold(answer.fistNumber - answer.secondNumber));
 } else if (answer.Operator === "Multiplication") {
-  console.log(answer.fistNumber * answer.secondNumber);
+  console.log(`\nAnswer: ${chalk.magenta.bold(answer.fistNumber)} * ${chalk.magenta.bold(answer.secondNumber)} =` , chalk.green.bold(answer.fistNumber * answer.secondNumber));
 } else if (answer.Operator === "Division") {
-  console.log(answer.fistNumber / answer.secondNumber);
+  console.log(`\nAnswer: ${chalk.magenta.bold(answer.fistNumber)} / ${chalk.magenta.bold(answer.secondNumber)} =` , chalk.green.bold(answer.fistNumber / answer.secondNumber));
 } else {
   console.log("Invalid Operator");
 }
